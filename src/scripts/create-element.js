7d55d44f00e92c0da3678c0ac5f531d856412708
Cameron Brown <jupiter49777@gmail.com>
2026-01-12 23:54:04 +0200

    feat: Add a function to handle creating nav list items

--- src/scripts/create-element.js
+++ src/scripts/create-element.js
@@ -1,3 +1,9 @@
-export const createElement = (() => {
-  return {}
+export const CreateElement = (() => {
+  const createNavListItem = (className, textContent) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add(className);
+    listItem.textContent = textContent;
+    return listItem;
+  }
+  return { createNavListItem }
 })()
