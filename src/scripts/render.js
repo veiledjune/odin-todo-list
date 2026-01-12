6022fbc8831890ef01bb0003b69a711c9b49a0d7
Cameron Brown <jupiter49777@gmail.com>
2026-01-13 00:01:20 +0200

    feat: Add renderNav function
    Add a function to handle rendering nav project list

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -1,3 +1,25 @@
-const Render = (() => {
-  
+import { App } from './app'
+import { CreateElement } from './create-element'
+import deleteIcon from '../icons/delete-icon.svg'
+
+export const Render = (() => {
+  const renderNav = () => {
+    const projectsArr = App.getProjects()
+    const navProjectList = document.querySelector('.nav-project-list');
+    navProjectList.textContent = '';
+    projectsArr.forEach(project => {
+      const listItem = CreateElement.createNavListItem('nav-project-item', project.title)
+      if (project.id === 'default-project') {
+        navProjectList.appendChild(listItem)
+        return;
+      }
+      const deleteBtn = CreateElement.createButton('project-delete-btn');
+      deleteBtn.dataset.id = project.id;
+      const deleteImg = CreateElement.createImage(deleteIcon);
+      deleteBtn.appendChild(deleteImg)
+      listItem.appendChild(deleteBtn);
+      navProjectList.appendChild(listItem)
+    })
+  }
+  return { renderNav }
 })()
