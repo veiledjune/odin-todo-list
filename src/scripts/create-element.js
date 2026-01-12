361d72447efd0456cc59cc4b676868e6fd1c57d7
Cameron Brown <jupiter49777@gmail.com>
2026-01-13 01:45:37 +0200

    feat: Remove createButton and createNavListItem functions in favor of createElement

--- src/scripts/create-element.js
+++ src/scripts/create-element.js
@@ -1,17 +1,4 @@
 export const CreateElement = (() => {
-  const createNavListItem = (className, textContent) => {
-    const listItem = document.createElement('li');
-    listItem.classList.add(className);
-    listItem.textContent = textContent;
-    return listItem;
-  }
-
-  const createButton = (className) => {
-    const button = document.createElement('button');
-    button.classList.add(className)
-    return button;
-  }
-
   const createImage = (src) => {
     const image = document.createElement('img');
     image.src = src;
@@ -25,5 +12,5 @@ export const CreateElement = (() => {
     element.textContent = textContent;
     return element;
   }
-  return { createNavListItem, createButton, createImage, createElement }
+  return { createImage, createElement }
 })()
