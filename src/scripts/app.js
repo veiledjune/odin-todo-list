2e1594d910e72a49dae20913c85fb77d24f98bdf
Cameron Brown <jupiter49777@gmail.com>
2026-01-13 01:11:11 +0200

    feat: Add deleteProject function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,7 @@ class Project {
   constructor(title, id = crypto.randomUUID()) {
     this.title = title;
     this.id = id;
-    this.todos = []
+    this.todos = [];
   }
 }
 
@@ -16,15 +16,15 @@ class Todo {
     this.check = false;
   }
   toggleCheck() {
-    this.check = !this.check
+    this.check = !this.check;
   }
 }
 
 export const App = (() => {
-  const defaultProject = new Project('My Day', 'default-project')
+  const defaultProject = new Project('My Day', 'default-project');
   const projectsArr = [defaultProject];
-  const getProjects = () => projectsArr
-  const addProject = (title) => projectsArr.push(new Project(title))
-  
-  return { getProjects, addProject }
+  const getProjects = () => projectsArr;
+  const addProject = (title) => projectsArr.push(new Project(title));
+  const deleteProject = (projectIndex) => projectsArr.splice(projectIndex, 1)
+  return { getProjects, addProject, deleteProject }
 })()
