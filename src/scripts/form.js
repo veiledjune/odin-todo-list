5772ed5f02efed243cfbab45949e7246fe670b92
Cameron Brown <jupiter49777@gmail.com>
2026-01-13 00:07:41 +0200

    feat: Add functions that handle opening and closing forms

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -1,3 +1,12 @@
 export const FormEvents = (() => {
-  
+  const openForm = (dialog) => {
+    const dialogElement = document.querySelector(dialog);
+    dialogElement.showModal();
+  }
+
+  const closeForm = (dialog) => {
+    const dialogElement = document.querySelector(dialog);
+    dialogElement.close();
+  }
+  return { openForm, closeForm }
 })();
