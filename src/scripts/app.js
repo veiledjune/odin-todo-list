7ea2a80ff402bcc8797818f83233aaf8be99279f
Cameron Brown <jupiter49777@gmail.com>
2026-01-12 23:20:03 +0200

    feat: Add Todo class
    Add a class that handles creating todo objects, add a method that toggles todo objects check value

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,21 @@
-class Project{
+class Project {
   constructor(title, id = crypto.randomUUID()) {
     this.title = title;
     this.id = id;
     this.todos = []
+  }
+}
+
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.id = crypto.randomUUID();
+    this.check = false;
+  }
+  toggleCheck() {
+    this.check = !this.check
   }
 }
