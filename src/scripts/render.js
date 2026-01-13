a9203f4ae10d07f4ee0a5399b7ea431b20ce0a91
Cameron Brown <jupiter49777@gmail.com>
2026-01-13 19:20:12 +0200

    refactor: Move state tracking logic from Render to App
    Separate state tracking logic from render for best practices

--- src/scripts/render.js
+++ src/scripts/render.js
@@ -5,98 +5,126 @@ import deleteIcon from '../icons/delete-icon.svg';
 import uncheckIcon from '../icons/uncheck-icon.svg';
 import checkIcon from '../icons/check-icon.svg';
 import editIcon from '../icons/edit-icon.svg';
-import { format } from 'date-fns';
 
 export const Render = (() => {
   const projectsArr = App.getProjects();
-  let currentProject = projectsArr[0];
   const renderNav = () => {
     const navProjectList = document.querySelector('.nav-project-list');
     navProjectList.textContent = '';
-    projectsArr.forEach(project => {
-      const listItem = CreateElement.createElement('li', 'nav-project-item', project.title);
-      listItem.dataset.id = project.id;
+    projectsArr.forEach((project) => {
+      const listItem = CreateElement.createElement(
+        'li',
+        'nav-project-item',
+        project.title
+      );
       listItem.addEventListener('click', () => {
-        currentProject = project;
-        const projectId = listItem.dataset.id;
-        const projectIndex = projectsArr.findIndex(project => project.id === projectId);
-        renderProject(projectIndex)
-      })
+        App.updateCurrentProject(project);
+        renderProject();
+      });
       if (project.id === 'default-project') {
-        navProjectList.appendChild(listItem)
+        navProjectList.appendChild(listItem);
         return;
       }
-      const deleteBtn = CreateElement.createElement('button', 'project-delete-btn');
-      deleteBtn.dataset.id = project.id;
+      const deleteBtn = CreateElement.createElement(
+        'button',
+        'project-delete-btn'
+      );
       deleteBtn.addEventListener('click', (e) => {
         e.stopPropagation();
-        if (currentProject === project) renderProject(0)
-        const projectId = deleteBtn.dataset.id;
-        const projectIndex = projectsArr.findIndex(project => project.id === projectId)
+        const currentProject = App.getCurrentProject();
+        if (project === currentProject) {
+          App.updateCurrentProject(projectsArr[0]);
+          renderProject(0);
+        }
+        const projectIndex = App.getProjectIndex(project.id);
         App.deleteProject(projectIndex);
         renderNav();
       });
       const deleteImg = CreateElement.createImage(deleteIcon);
-      deleteBtn.appendChild(deleteImg)
+      deleteBtn.appendChild(deleteImg);
       listItem.appendChild(deleteBtn);
-      navProjectList.appendChild(listItem)
+      navProjectList.appendChild(listItem);
     });
-  }
+  };
 
-  const renderProject = (projectIndex) => {
-    const project = projectsArr[projectIndex];
+  const renderProject = () => {
+    const currentProject = App.getCurrentProject();
     const projectTitleElement = document.querySelector('.project-title');
-    projectTitleElement.textContent = project.title;
+    projectTitleElement.textContent = currentProject.title;
     const todoListContainer = document.querySelector('.todo-list-container');
     todoListContainer.textContent = '';
-    const todoAddBtn = document.querySelector('.todo-add-btn');
-    todoAddBtn.dataset.id = project.id;
-    project.todos.forEach(todo => {
+    currentProject.todos.forEach((todo) => {
       const todoDiv = CreateElement.createElement('div', 'todo-card');
-      const todoCheckBtn = CreateElement.createElement('button', 'todo-check-btn');
+      const todoCheckBtn = CreateElement.createElement(
+        'button',
+        'todo-check-btn'
+      );
       todoCheckBtn.addEventListener('click', () => {
         todo.toggleCheck();
-        renderProject(projectIndex);
-      })
-      const todoCheckimg = todo.check ? 
-        CreateElement.createImage(checkIcon) : CreateElement.createImage(uncheckIcon);
-      todoCheckBtn.appendChild(todoCheckimg)
-      const todoTitle = CreateElement.createElement('h4', 'todo-title', `Title: ${todo.title}`)
-      const todoDescription = CreateElement.createElement('p', 'todo-description', todo.description);
-      const todoDueDate= CreateElement.createElement('span', 'todo-due-date', `Due: ${todo.dueDate}`);
-      const todoPriority = CreateElement.createElement('span', 'todo-priority', `Priority: ${todo.priority}`);
+        renderProject();
+      });
+      const todoCheckimg = todo.check
+        ? CreateElement.createImage(checkIcon)
+        : CreateElement.createImage(uncheckIcon);
+      todoCheckBtn.appendChild(todoCheckimg);
+      const todoTitle = CreateElement.createElement(
+        'h4',
+        'todo-title',
+        `Title: ${todo.title}`
+      );
+      const todoDescription = CreateElement.createElement(
+        'p',
+        'todo-description',
+        todo.description
+      );
+      const todoDueDate = CreateElement.createElement(
+        'span',
+        'todo-due-date',
+        `Due: ${todo.dueDate}`
+      );
+      const todoPriority = CreateElement.createElement(
+        'span',
+        'todo-priority',
+        `Priority: ${todo.priority}`
+      );
       if (todo.priority === 'Low') {
-        todoPriority.classList.add('--low')
+        todoPriority.classList.add('--low');
       } else todoPriority.classList.add('--high');
-      const todoEditBtn = CreateElement.createElement('button', 'todo-edit-btn');
+      const todoEditBtn = CreateElement.createElement(
+        'button',
+        'todo-edit-btn'
+      );
       todoEditBtn.addEventListener('click', () => {
+        App.updateActiveTodo(todo);
         FormEvents.openForm('.edit-dialog');
-        const editFormSubmitButton = document.querySelector('.edit-form-add-btn');
-        editFormSubmitButton.dataset.id = todo.id;
-        const titleInput = document.getElementById('edit-title');
-        const descriptionInput = document.getElementById('edit-description');
-        const dueDateInput = document.getElementById('edit-due-date');
-        titleInput.value = todo.title;
-        descriptionInput.value = todo.description;
-        console.log(todo.dueDate);
-        dueDateInput.valueAsDate = new Date();
-      })
+        FormEvents.editFormButtonEvents(todo);
+      });
       const todoEditIcon = CreateElement.createImage(editIcon);
       todoEditBtn.appendChild(todoEditIcon);
-      const todoDeleteBtn = CreateElement.createElement('button', 'todo-delete-btn');
-      todoDeleteBtn.dataset.id = todo.id;
+      const todoDeleteBtn = CreateElement.createElement(
+        'button',
+        'todo-delete-btn'
+      );
       todoDeleteBtn.addEventListener('click', () => {
-        const todoId = todoDeleteBtn.dataset.id;
-        const todoIndex = project.todos.findIndex(todo => todo.id === todoId);
-        App.deleteTodo(project, todoIndex);  
-        renderProject(projectIndex);
-      })
+        const currentProject = App.getCurrentProject();
+        const currentProjectIndex = App.getCurrentProjectIndex();
+        const todoIndex = App.getTodoIndex(currentProject, todo);
+        App.deleteTodo(currentProject, todoIndex);
+        renderProject(currentProjectIndex);
+      });
       const todoDeleteImg = CreateElement.createImage(deleteIcon);
       todoDeleteBtn.appendChild(todoDeleteImg);
-      todoDiv.append(todoCheckBtn, todoTitle, todoDueDate, todoPriority, todoEditBtn, todoDeleteBtn, todoDescription)
+      todoDiv.append(
+        todoCheckBtn,
+        todoTitle,
+        todoDueDate,
+        todoPriority,
+        todoEditBtn,
+        todoDeleteBtn,
+        todoDescription
+      );
       todoListContainer.appendChild(todoDiv);
-    })
-    
-  }
-  return { renderNav, renderProject }
-})()
+    });
+  };
+  return { renderNav, renderProject };
+})();
