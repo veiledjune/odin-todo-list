3c964869a0c1616928fe99b9849939da8e4e6420
Cameron Brown <jupiter49777@gmail.com>
2026-01-13 19:21:48 +0200

    feat: Import required functions into index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './styles.css'
+import './styles.css';
+
+import { Render } from './scripts/render';
+import { FormEvents } from './scripts/form';
+Render.renderNav();
+Render.renderProject(0);
+FormEvents.formEvents(
+  '.project-dialog',
+  '.project-form',
+  '.project-add-btn',
+  '.project-form-close-btn',
+  '.project-form-add-btn'
+);
+FormEvents.formEvents(
+  '.todo-dialog',
+  '.todo-form',
+  '.todo-add-btn',
+  '.todo-form-close-btn',
+  '.todo-form-add-btn'
+);
+FormEvents.formEvents(
+  '.edit-dialog',
+  '.edit-form',
+  '.todo-edit-btn',
+  '.edit-form-close-btn',
+  '.edit-form-add-btn'
+);
