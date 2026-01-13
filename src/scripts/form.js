df767dd6c467e7890b6f1ee42e63546255d1deea
Cameron Brown <jupiter49777@gmail.com>
2026-01-13 02:48:25 +0200

    feat: Add edit form event function

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -42,7 +42,7 @@ export const FormEvents = (() => {
         Render.renderNav();
       } else if (formSelector.includes('todo')) {
         todoFormEvents();
-      }
+      } else editFormEvents();
       resetForm(formSelector);
       closeForm(dialogSelector);
     })
@@ -63,6 +63,20 @@ export const FormEvents = (() => {
     const todoPriority = document.getElementById('todo-priority').value;
     App.addTodo(projectIndex, todoTitle, todoDescription, todoDueDate, todoPriority);
     Render.renderProject(projectIndex)
+  }
+
+  const editFormEvents = () => {
+    const projectsArr = App.getProjects();
+    const projectId = document.querySelector('.todo-add-btn').dataset.id;
+    const projectIndex = projectsArr.findIndex(project => project.id === projectId);
+    const todoId = document.querySelector('.edit-form-add-btn').dataset.id;
+    const todoIndex = projectsArr[projectIndex].todos.findIndex(todo => todo.id === todoId);
+    const title = document.getElementById('edit-title').value;
+    const description = document.getElementById('edit-description').value;
+    const dueDate = document.getElementById('edit-due-date').value;
+    const priority = document.getElementById('edit-priority').value;
+    App.editTodo(projectIndex, todoIndex, title, description, dueDate, priority)
+    Render.renderProject(projectIndex);
   }
   return { openForm, closeForm, formEvents }
 })();
