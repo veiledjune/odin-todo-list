9a91ae8bdc9ffb9f2c8c726f24d0d5db10fe9536
Cameron Brown <veiledjune@gmail.com>
2026-03-24 16:04:38 +0200

    feat: Intialize app by importing and calling starter functions inside index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles.css';
+import { events } from './scripts/events';
+import { render } from './scripts/render';
+
+events.formEvents();
+render.renderNav();
+render.renderProject();
+events.menuButtonEvent();
